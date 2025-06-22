06e67831abd7466105218a204048f52894d2b6cc
“Avishek <avishekroy87@gmail.com>
2025-06-22 18:59:43 +0530

    firstcommit1.0

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-o^x2r43coc*td1rnw&din3xj)1q6gk6n0&v71^7ow8l_m6=q04"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
